aec22d83f015301b78b1b52afa6a2384d92ca0e7
apple <apple@platonademacbook-pro.local>
2016-05-21 14:52:15 +0800

    初始化

---
+++ 自定义系统菜单控件/自定义系统菜单控件/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义系统菜单控件
+//
+//  Created by apple on 16/5/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义系统菜单控件/自定义系统菜单控件/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义系统菜单控件
+//
+//  Created by apple on 16/5/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
